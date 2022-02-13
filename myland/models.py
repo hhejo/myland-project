15871f500dfbe0c3d5ddaee10a7314e929dbe4d7
hhejo <litsmainjof@gmail.com>
2022-02-13 16:55:33 +0900

    220213 user model migrate upgrade

--- myland/models.py
+++ myland/models.py
@@ -6,3 +6,10 @@ class Post(db.Model):
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
