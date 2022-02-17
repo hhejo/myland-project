f1a9f27de608ada1acbaa052004a73ca9fa6ec96
hhejo <litsmainjof@gmail.com>
2022-02-17 19:58:00 +0900

    220217 User

--- myland/views/auth_views.py
+++ myland/views/auth_views.py
@@ -8,11 +8,11 @@ from ..models import User
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/signup/', methods=['GET', 'POST'])
+@bp.route('/signup/', methods=('GET', 'POST'))
 def signup():
     form = UserCreateForm()
     if request.method == 'POST' and form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.date).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if not user:
             user = User(username=form.username.data, password=generate_password_hash(form.password1.data), email=form.email.data)
             db.session.add(user)
