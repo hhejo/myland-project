01adbecf86fb67e3c1edf67a35e64bd7b7696303
hhejo <litsmainjof@gmail.com>
2022-03-03 07:49:19 +0900

    220303

--- myland/models.py
+++ myland/models.py
@@ -8,6 +8,7 @@ class Post(db.Model):
     create_date = db.Column(db.DateTime(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('post_set'))
+    modify_date = db.Column(db.DateTime(), nullable=True)
 
 
 class User(db.Model):
@@ -15,3 +16,4 @@ class User(db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    modify_date = db.Column(db.DateTime(), nullable=True)
